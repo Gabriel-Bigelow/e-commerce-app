f572e9c3b0611699361eae38c1e49d9f0ee7d82c
Gabriel-Bigelow <gabebigelow@gmail.com>
2023-02-20 15:34:54 -0500

    remove / from CORS origin

--- functions/app.js
+++ functions/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(cors({
-    origin: "https://salttech.netlify.app/",
+    origin: "https://salttech.netlify.app",
     credentials: true
 }));
 
